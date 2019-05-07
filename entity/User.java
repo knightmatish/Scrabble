e7fe0447a0c31ee063a71293339314988aa35382
Nitish Mathur <49384284+knightmatish@users.noreply.github.com>
2019-05-08 01:49:19 +1000

    Add files via upload

---
+++ entity/User.java
@@ -0,0 +1,81 @@
+package entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	private static final long serialVersionUID = 7140074144500173010L;
+	private String ip;
+	private String name;
+	private int port;
+
+	public User(String ip, String name, int port) {
+		super();
+		this.ip = ip;
+		this.name = name;
+		this.port = port;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ip == null) ? 0 : ip.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + port;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (ip == null) {
+			if (other.ip != null)
+				return false;
+		} else if (!ip.equals(other.ip))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (port != other.port)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [ip=" + ip + ", name=" + name + ", port=" + port + "]";
+	}
+
+}
